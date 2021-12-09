b1ab82631104c44046697bcb70f7ff98b0121739
Here East <nastiapiven@gmail.com>
2021-12-09 23:21:49 +0100

    feat: added progress bar html/css

---
+++ src/js/tracks.js
@@ -0,0 +1,22 @@
+export const tracks = [
+    {      
+      title: "Why Do You Only Call Me When You Are High",
+      src: "../sounds/Arctic_Monkeys-Why_Do_You_Only_Call_Me_When_You_Are_High.mp3",
+      duration: "02:41"
+    }, 
+    {      
+      title: "Arabella",
+      src: "../sounds/Arctic_Monkeys-Arabella.mp3",
+      duration: "03:27"
+    }, 
+    {      
+      title: "R U Mine",
+      src: "../sounds/Arctic_Monkeys-R_U_Mine_.mp3",
+      duration: "03:22"
+    }, 
+    {      
+      title: "Crying Lightning",
+      src: "../sounds/Arctic_Monkeys-Crying_Lightning.mp3",
+      duration: "03:45"
+    }
+  ];
