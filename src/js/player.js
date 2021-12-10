1f13cb1f09bd88da4d39e3c3c2d42be46f85d8c7
Here East <nastiapiven@gmail.com>
2021-12-11 00:05:40 +0100

    feat: added progress bar drag progress

--- src/js/player.js
+++ src/js/player.js
@@ -5,7 +5,8 @@ const audio = document.createElement("audio");
 player.append(audio);
 
 let trackNum = 0;
-let isPlaying = false;
+let currentAudioTime = 0;
+let value = 0;
 
 if(!audio.paused) isPlaying = true;
 
@@ -16,9 +17,11 @@ const playlist = document.querySelector(".playlist");
 function buildPlaylist() {
     tracks.forEach(track => {
         const li = document.createElement("li");
-        li.classList.add("track-name");
+        const a = document.createElement("a");
+        a.classList.add("track-name");
 
-        li.innerText = track.title;
+        a.innerText = track.title;
+        li.append(a);
         playlist.append(li);
     })
 }
@@ -26,8 +29,9 @@ function buildPlaylist() {
 buildPlaylist();
 
 
-
-// CONTROLS
+//////////////
+// CONTROLS //
+//////////////
 
 const playBtn = document.querySelector(".play_btn");
 const nextTrackBtn = document.querySelector(".next-track_btn");
@@ -41,23 +45,18 @@ function toggleAudio() {
     if(audio.paused) {
         audio.src = tracks[trackNum].src;
         audio.play();
+        playBtn.classList.add("pause_btn");
     } else {
         audio.pause();
+        playBtn.classList.remove("pause_btn");
     }
 
-    playBtn.classList.toggle("pause_btn");
     changeListStyle();
 }
 
-// function playAudio() {
-//     audio.src = tracks[trackNum].src;
-//     audio.play();
-//     playBtn.classList.toggle("pause_btn");
-
-//     changeListStyle();
-// }
-
 function playNextAudio() {
+    if(audio.paused) return;
+
     trackNum += 1;
     if(trackNum > tracks.length - 1) trackNum = 0;
     audio.src = tracks[trackNum].src;
@@ -67,6 +66,8 @@ function playNextAudio() {
 }
 
 function playPrevAudio() {
+    if(audio.paused) return;
+
     trackNum -= 1;
     if(trackNum < 0) trackNum = tracks.length - 1;
     audio.src = tracks[trackNum].src;
@@ -76,9 +77,26 @@ function playPrevAudio() {
 }
 
 
+//////////////////
+// PROGRESS BAR //
+//////////////////
+
+const progressBar = document.querySelector(".progress-bar");
+
+progressBar.addEventListener("input", changeProgress);
+console.log(progressBar.value);
+
+function changeProgress() {
+    value = progressBar.value;
+    let progress = `linear-gradient(to right, #000000 0%, #000000 ${value}%, #D4D4D4 ${value}%, #D4D4D4 100%)`;
+    progressBar.style.background = progress;
+}
 
 
-// PLAY PLAYLIST
+
+///////////////////
+// PLAY PLAYLIST //
+///////////////////
 
 const trackNames = Array.from(document.querySelectorAll(".track-name"));
 console.log(trackNames);
@@ -89,14 +107,7 @@ function playAudioList(event) {
     let i = trackNames.indexOf(event.target);
     trackNum = i;
 
-    if(!audio.paused) {
-        audio.pause();
-        playBtn.classList.remove("pause_btn");
-    }
-    audio.src = tracks[trackNum].src;
-    audio.play();
-    playBtn.classList.add("pause_btn");
-
+    toggleAudio();
     changeListStyle();
 }
 
